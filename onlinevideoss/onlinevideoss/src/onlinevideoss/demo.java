bb6905ee08b223bb073b332864112dadb73e8655
vinayvarmapenmetsa <vinayvarmapenmetsa@users.noreply.github.com>
2020-05-25 16:45:04 -0400

    initial draft

---
+++ onlinevideoss/onlinevideoss/src/onlinevideoss/demo.java
@@ -0,0 +1,15 @@
+package onlinevideoss;
+
+public class demo {
+
+	public static void main(String[] args) {
+		int finalTotal = sum(5);
+		System.out.println(finalTotal);
+
+	}
+public static int sum(int n)
+{
+int total = (n*(n+5))/2;
+return total;
+}
+}
